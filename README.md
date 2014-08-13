a16753a03e5e00c6eb00bea41edc7fa374566a00
Chris Cowan <chris@chriscowan.us>
2014-08-13 15:35:06 -0700

    intial import

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
 esvm
 ====
 
-Elasticsearch Version Manager
+**Elasticsearch Version Manager** is a command line application used for development to manage different versions of Elasticsearch. It should not be used in production since it's probably a bad idea to wrap a process with Node.js. But never the less it's extremely useful if you need to develop against multiple versions of Elasticsearch (I'm looking at you Kibana team).
+
+## Installation
+```
+npm install esvm -g
+```
+
+### Usage
+```
+Usage: esvm [options] <version>
+
+Options:
+
+  -h, --help       output usage information
+  -V, --version    output the version number
+  -p, --purge      purge the data directory
+  -f, --fresh      preform a fresh install
+  -b, --branch     install from a branch release
+  -n, --nodes <n>  the number of nodes to star
+```
+
+### Configuration
+
+**esvm** will look for an .esvmrc file in the current working directory. If it doesn't find one there it will continue to walk up the directory tree until it does. There are two main sections for the configuration file:
+
+* `defaults` - The defaults to apply to every instance
+* `clusters` - The named clusters. See example
+
+Both the defaults and named clusters will take the following options:
+
+* `version`   - The semver statment for the released version of Elasticsearch to install (This will override branch and binary).
+* `branch`    - The nightly branch to install
+* `binary`    - The path to the tarball to use. This can either be URL or file path.
+* `directory` - The directory where everything is installed. If the directory doesn't exist it will be created.
+* `plugins`   - The plugins to install. This should be an array of plugin install directives.
+* `purge`     - Purge the data directory when starting the server (Default: false).
+* `fresh`     - Remove the current copy before installing a new copy (Default: false).
+* `nodes`     - The number of nodes to start. This can either be a number or an array of config objects (1 per node)
+* `config`    - The config to start the server with.
+
+### Example .esvmrc
+
+```json
+{
+  "defaults": {
+    "plugins": ["elasticsearch/marvel/latest"]
+  },
+  "clusters": {
+    "2node": {
+      "branch": "master",
+      "nodes": 2
+    }
+  }
+}
+```
---
+++ bin/esvm.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+var libesvm     = require('libesvm');
+var clc         = require('cli-color');
+var moment      = require('moment');
+var _           = require('lodash');
+var ProgressBar = require('progress');
+var RcLoader    = require('rcloader');
+var os          = require('os');
+var commander   = require('commander');
+var packageInfo = require('../package.json');
+
+var rcloader = new RcLoader('.esvmrc');
+var config = rcloader.for(process.cwd());
+
+var defaults = _.defaults(config.defaults, {
+  version: '*',
+  directory: process.env.HOME+'/.esvm',
+  plugins: [],
+  purge: false, // Purge the data directory
+  fresh: false, // Download a fresh copy
+  nodes: 1,
+  config: {
+    cluster: { name: os.hostname() }
+  }
+});
+
+var levels = {
+  INFO: clc.green,
+  DEBUG: clc.cyan,
+  WARN: clc.yellow,
+  FATAL: clc.magentaBright,
+  ERROR: clc.white.bgRed
+};
+
+
+commander
+	.version(packageInfo.version)
+	.usage('[options] <version>')
+	.option('-p, --purge', 'purge the data directory')
+	.option('-f, --fresh', 'preform a fresh install')
+	.option('-b, --branch', 'install from a branch release')
+	.option('-n, --nodes <n>', 'the number of nodes to start', parseInt)
+	.parse(process.argv);
+
+var version = commander.args[0];
+var options = {};
+
+// If the version is a named config use that otherwise
+// use the version as the actual version
+if (version) {
+	if (config.clusters[version]) {
+		options = config.clusters[version];
+	} else if (commander.branch) {
+		delete defaults.version;
+		options = { branch: version }
+	} else {
+		options = { version: version };
+	}
+}
+
+// Assign the overrides from the CLI options
+_.assign(options, _.pick(commander, ['fresh', 'nodes', 'purge']));
+
+// Set the defaults
+options = _.defaults(options, defaults);
+var cluster = libesvm.createCluster(options);
+
+// Setup the logging
+cluster.on('log', function (log) {
+  var bar, pattern;
+  if (log.type === 'progress') {
+    pattern = log.op + ' [:bar] :percent :etas';
+    bar = new ProgressBar(pattern, {
+      complete: '=',
+      incomplete: ' ',
+      width: 80,
+      clear: true,
+      total: log.total
+    });
+    log.on('progress', _.bindKey(bar, 'tick'));
+    return;
+  }
+  var level = levels[log.level] || function (msg) { return msg; };
+  var message = clc.blackBright(moment(log.timestamp).format('lll'));
+  message += ' '+level(log.level);
+  if (log.node) {
+    message += ' ' + clc.magenta(log.node);
+  }
+  message += ' ' + clc.yellow(log.type) + ' ';
+  message += log.message;
+  console.log(message);
+});
+
+// Install and then start the cluster
+cluster.install().then(function () {
+ return cluster.installPlugins();
+}).then(function () {
+ return cluster.start();
+}).then(function () {
+  process.on('SIGINT', function () {
+    cluster.shutdown().then(function () {
+      console.log(clc.black.bgWhite("Bye Bye!"));
+      process.exit();
+    });
+  });
+  process.stdin.read();
+}).catch(function (err) {
+ console.log('Oops', err.stack);
+});
